d7373077bebd1b56bfd4cb3af91f4b49393d68b3
jillurcf <jrtushar@gmail.com>
2024-09-26 14:12:44 +0600

    Few chnages in carousal of signup page

--- src/app/auth/signup/page.js
+++ src/app/auth/signup/page.js
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
-import { Form, Input, Button, Checkbox } from "antd";
-import { GoogleOutlined } from "@ant-design/icons";
+import { Form, Input, Button } from "antd";
 import AuthLayout from "@/components/AuthLayout";
 import Link from "next/link";
 import Image from "next/image";
 import signupimage from "../../../public/images/logo.png";
 import googleicon from "../../../public/images/google.png";
-import SignupCarosel from "@/components/utils/SignupCarosel";
-const signup = () => {
+import SignupCarousel from "@/components/utils/SignupCarosel";
+
+const Signup = () => {
   const onFinish = (values) => {
     console.log("Success:", values);
   };
@@ -19,21 +19,17 @@ const signup = () => {
 
   return (
     <AuthLayout>
-      <div className="xl:flex lg:flex  flex-row items-center justify-between container mx-auto   px-4">
-
-      <div className="w-full h-full xl:pb-0 md:pb-0 pb-12 ">
-        <div className="flex justify-start items-center mb-4 container mx-auto  p-4">
-          <Image src={signupimage} />
-        </div>
-
-        <div className="max-w-md mx-auto">
-          <div className="text-start my-8">
-            <h1 className="text-3xl font-bold mb-4">Sign up</h1>
-            <h3 className="text-[#475467] text-[16px]">
-              Please enter your details.
-            </h3>
+      <div className="xl:flex lg:flex  max-h-screen h-screen flex-row items-center justify-between mx-auto  overflow-hidden">
+        {/* Left Section */}
+        <div className="w-6/12 h-full xl:pb-0 md:pb-0 pb-12 flex flex-col justify-center">
+          <div className="flex justify-start items-center mb-4">
+            <Image src={signupimage} alt="Logo" />
           </div>
-          <div className="flex justify-start items-center ">
+          <div className="max-w-md mx-auto">
+            <div className="text-start my-8">
+              <h1 className="text-3xl font-bold mb-4">Sign up</h1>
+              <h3 className="text-[#475467] text-[16px]">Please enter your details.</h3>
+            </div>
             <Form
               name="signin"
               layout="vertical"
@@ -43,21 +39,10 @@ const signup = () => {
             >
               <Form.Item
                 label={
-                  <label
-                    htmlFor="name"
-                    className="text-sm text-[#344054] font-medium"
-                  >
-                    Name*
-                  </label>
+                  <label className="text-sm text-[#344054] font-medium">Name*</label>
                 }
                 name="name"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your name!",
-                    type: "text",
-                  },
-                ]}
+                rules={[{ required: true, message: "Please input your name!" }]}
                 required={false}
               >
                 <Input
@@ -65,23 +50,13 @@ const signup = () => {
                   placeholder="Enter your Name"
                 />
               </Form.Item>
+
               <Form.Item
                 label={
-                  <label
-                    htmlFor="email"
-                    className="text-sm text-[#344054] font-medium"
-                  >
-                    Email*
-                  </label>
+                  <label className="text-sm text-[#344054] font-medium">Email*</label>
                 }
                 name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your email!",
-                    type: "email",
-                  },
-                ]}
+                rules={[{ required: true, message: "Please input your email!", type: "email" }]}
                 required={false}
               >
                 <Input
@@ -92,20 +67,12 @@ const signup = () => {
 
               <Form.Item
                 label={
-                  <label
-                    htmlFor="password"
-                    className="text-sm text-[#344054] font-medium"
-                  >
-                    Password*
-                  </label>
+                  <label className="text-sm text-[#344054] font-medium">Password*</label>
                 }
                 name="password"
                 rules={[
                   { required: true, message: "Please input your password!" },
-                  {
-                    min: 8,
-                    message: "Password must be at least 8 characters long.",
-                  }, // Validation rule for minimum length
+                  { min: 8, message: "Password must be at least 8 characters long." },
                 ]}
                 required={false}
                 extra={<p className="text-[16px] font-normal text-[#475467] pt-2">Must be at least 8 characters.</p>}
@@ -116,51 +83,35 @@ const signup = () => {
                 />
               </Form.Item>
 
-         
-
               <Form.Item>
-                <Button
-                  className="text-[#FFFFFF] text-[16px] font-semibold p-6"
-                  size="large"
-                  type="primary"
-                  htmlType="submit"
-                  block
-                >
-                 Create account
+                <Button className="text-[#FFFFFF] text-[16px] font-semibold p-6" size="large" type="primary" htmlType="submit" block>
+                  Create account
                 </Button>
               </Form.Item>
 
-              {/* Google Sign In Button */}
               <Form.Item>
-                <Button
-                  block
-                  className="btn-google text-[#344054] text-[16px] font-semibold p-6 hover:border-[#344054] hover:bg-[#344054] hover:text-[#FFFFFF]"
-                  style={{ marginBottom: "10px" }}
-                >
-                  <Image src={googleicon} width={24} height={24} />
+                <Button block className="btn-google text-[#344054] text-[16px] font-semibold p-6 hover:bg-[#344054] hover:text-[#FFFFFF]" style={{ marginBottom: "10px" }}>
+                  <Image src={googleicon} width={24} height={24} alt="Google Icon" />
                   Sign up with Google
                 </Button>
               </Form.Item>
             </Form>
-          </div>
-          <div className="text-center lg:mt-4">
-          Already have an account?{" "}
-            <Link href="/auth/login">
-              <span className="text-[#195671] font-semibold hover:underline">
-                Log in
-              </span>
-            </Link>
+            <div className="text-center lg:mt-4">
+              Already have an account?{" "}
+              <Link href="/auth/login">
+                <span className="text-[#195671] font-semibold hover:underline">Log in</span>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="xl:w-1/2 lg:w-1/2  w-full  ">
-        <SignupCarosel/>
+        {/* Right Section */}
+        <div className=" h-screen overflow-hidden w-6/12">
+          <SignupCarousel />
+        </div>
       </div>
-      </div>
-
     </AuthLayout>
   );
 };
 
-export default signup;
+export default Signup;
--- src/components/utils/SignupCarosel.jsx
+++ src/components/utils/SignupCarosel.jsx
@@ -4,6 +4,7 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import SignupImage from "../../public/images/signupimage2.png"; // Update the image path if necessary
 import girlimage from "../../public/images/medium-shot-smiley-woman.jpg";
+
 const SignupCarousel = () => {
   const carouselRef = useRef(null);
 
@@ -18,10 +19,10 @@ const SignupCarousel = () => {
   };
 
   return (
-    <div className="relative xl:h-[832px] h-fit lg:p-0 md:p-0  ">
+    <div className="relative h-full lg:p-0 md:p-0 overflow-hidden"> {/* Added h-screen and overflow-hidden */}
       {/* Custom Left Arrow */}
       <div
-        className="absolute xl:bottom-0 lg:bottom-8 bottom-28 xl:left-[60%] lg:left-[50%] md:left-[40%] left-[40%] transform -translate-y-1/2 bg-transparent border rounded-full h-[56px] w-[56px] flex items-center justify-center z-10 cursor-pointer"
+        className="absolute xl:bottom-8 lg:bottom-8 bottom-8 xl:left-[60%] lg:left-[40%] left-[40%] transform -translate-y-1/2 bg-transparent border rounded-full h-[56px] w-[56px] flex items-center justify-center z-10 cursor-pointer"
         onClick={prevSlide}
       >
         <ArrowLeftOutlined style={{ color: "#fff", fontSize: "20px" }} />
@@ -30,22 +31,22 @@ const SignupCarousel = () => {
       {/* Carousel */}
       <Carousel ref={carouselRef} dots={false} arrows={false} infinite>
         {/* First Slide */}
-        <div className="relative h-[920px] w-full ">
+        <div className="relative h-screen w-full overflow-hidden"> {/* Updated to h-full */}
           {/* Image */}
           <Image
-            className="w-full h-[920px] rounded-bl-[10%] rounded-tl-[10%] object-cover"
+            className="w-full h-full object-cover rounded-bl-[10%] rounded-tl-[10%]" // Ensures the image covers the entire container
             src={SignupImage}
             alt="carousel1"
           />
 
           {/* Black Overlay */}
-          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4]   top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%] ">
+          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4] top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%]">
             <div className="xl:p-11 lg:p-11 p-6">
               <h1 className="xl:text-[36px] lg:text-[28px] text-[24px] text-[#FCFCFD] font-bold mb-4">
                 “We’ve been using PANTAGONOSTIS to kick start every new course
                 and can’t imagine working without it.”
               </h1>
-              <h3 className="xl:text-xl  text-sm font-bold text-[#FFFFFF] font-Inter xl:py-4 lg:py-4">
+              <h3 className="xl:text-xl text-sm font-bold text-[#FFFFFF] font-Inter xl:py-4 lg:py-4">
                 Amélie Laurent
               </h3>
               <h3 className="xl:text-lg text-sm font-Inter text-[#FFFFFF] font-medium">
@@ -59,56 +60,57 @@ const SignupCarousel = () => {
         </div>
 
         {/* Second Slide */}
-
-        <div className="relative h-fit w-full ">
+        <div className="relative h-screen w-full overflow-hidden"> {/* Updated to h-full */}
           {/* Image */}
           <Image
-            className="w-full h-[920px] rounded-bl-[10%] rounded-tl-[10%] object-cover"
-            src={girlimage}
+            className="w-full h-full object-cover rounded-bl-[10%] rounded-tl-[10%]" // Ensures the image covers the entire container
+            src={SignupImage}
             alt="carousel1"
           />
 
           {/* Black Overlay */}
-          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4]   top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%] ">
-            <div className="xl:p-11 lg:p-11 p-6">
-              <h1 className="xl:text-[36px] lg:text-[28px] text-[24px] text-[#FCFCFD] font-bold mb-4">
-              “We’ve been using PANTAGONOSTIS to kick start every new course and can’t imagine working without it.”
-              </h1>
-              <h3 className="text-xl font-bold text-[#FFFFFF] font-Inter py-4">
-                Amélie Laurent
-              </h3>
-              <h3 className="text-lg font-Inter text-[#FFFFFF] font-medium">
-                Lead Designer, Layers
-              </h3>
-              <h3 className="text-[16px] text-[#FFFFFF] font-medium font-Inter opacity-70">
-                Web Development Agency
-              </h3>
-            </div>
-          </div>
-        </div>
-        {/* Third Slide */}
-        <div className="relative h-fit w-full ">
-          {/* Image */}
-          <Image
-            className="w-full h-[920px] rounded-bl-[10%] rounded-tl-[10%] object-cover"
-            src={girlimage}
-            alt="carousel1"
-          />
-
-          {/* Black Overlay */}
-          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4]   top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%] ">
+          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4] top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%]">
             <div className="xl:p-11 lg:p-11 p-6">
               <h1 className="xl:text-[36px] lg:text-[28px] text-[24px] text-[#FCFCFD] font-bold mb-4">
                 “We’ve been using PANTAGONOSTIS to kick start every new course
                 and can’t imagine working without it.”
               </h1>
-              <h3 className="text-xl font-bold text-[#FFFFFF] font-Inter py-4">
+              <h3 className="xl:text-xl text-sm font-bold text-[#FFFFFF] font-Inter xl:py-4 lg:py-4">
                 Amélie Laurent
               </h3>
-              <h3 className="text-lg font-Inter text-[#FFFFFF] font-medium">
+              <h3 className="xl:text-lg text-sm font-Inter text-[#FFFFFF] font-medium">
                 Lead Designer, Layers
               </h3>
-              <h3 className="text-[16px] text-[#FFFFFF] font-medium font-Inter opacity-70">
+              <h3 className="xl:text-[16px] text-sm text-[#FFFFFF] font-medium font-Inter opacity-70">
+                Web Development Agency
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        {/* Third Slide */}
+        <div className="relative h-screen w-full overflow-hidden"> {/* Updated to h-full */}
+          {/* Image */}
+          <Image
+            className="w-full h-full object-cover rounded-bl-[10%] rounded-tl-[10%]" // Ensures the image covers the entire container
+            src={SignupImage}
+            alt="carousel1"
+          />
+
+          {/* Black Overlay */}
+          <div className="absolute inset-0 bg-gradient-to-t from-[#000000b4] top-[#00000080] flex flex-col justify-end items-start p-4 text-white text-start font-Merriweather rounded-bl-[10%] rounded-tl-[10%]">
+            <div className="xl:p-11 lg:p-11 p-6">
+              <h1 className="xl:text-[36px] lg:text-[28px] text-[24px] text-[#FCFCFD] font-bold mb-4">
+                “We’ve been using PANTAGONOSTIS to kick start every new course
+                and can’t imagine working without it.”
+              </h1>
+              <h3 className="xl:text-xl text-sm font-bold text-[#FFFFFF] font-Inter xl:py-4 lg:py-4">
+                Amélie Laurent
+              </h3>
+              <h3 className="xl:text-lg text-sm font-Inter text-[#FFFFFF] font-medium">
+                Lead Designer, Layers
+              </h3>
+              <h3 className="xl:text-[16px] text-sm text-[#FFFFFF] font-medium font-Inter opacity-70">
                 Web Development Agency
               </h3>
             </div>
@@ -118,7 +120,7 @@ const SignupCarousel = () => {
 
       {/* Custom Right Arrow */}
       <div
-        className="absolute right-[20%] xl:bottom-0 lg:bottom-8 bottom-28 transform -translate-y-1/2 bg-transparent border rounded-full h-[56px] w-[56px] flex items-center justify-center z-10 cursor-pointer"
+        className="absolute right-[10%] xl:bottom-8 lg:bottom-8 bottom-8 transform -translate-y-1/2 bg-transparent border rounded-full h-[56px] w-[56px] flex items-center justify-center z-10 cursor-pointer"
         onClick={nextSlide}
       >
         <ArrowRightOutlined style={{ color: "#fff", fontSize: "25px" }} />

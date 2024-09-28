a5f2889afa81872d1fa8ffad547495d2d721cd4e
Mahedihasanimam <mdmehedihasen678@gmail.com>
2024-09-28 18:13:41 +0600

    fix some issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
 import "./globals.css";
 import Navbar from "@/components/share/Navbar";
 import Footer from "@/components/share/Footer";
-import { usePathname } from "next/navigation"; 
-import { Spin } from 'antd';
+import { usePathname } from "next/navigation";
+import { Spin } from "antd";
 import Image from "next/image";
-import logo from '../public/images/logo.png'
+import logo from "../public/images/logo.png";
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
     "/auth/otpverification",
     "/auth/forgetpassword",
     "/auth/signup/intarest",
-    "/auth/signup/categories"
+    "/auth/signup/categories",
   ];
   const isAuthRoute = authRoutes.includes(pathname);
   const isFooter = footerCriteria.includes(pathname);
@@ -32,40 +32,49 @@ export default function RootLayout({ children }) {
           clearInterval(interval);
           return 100;
         }
-        return prev + 10; 
+        return prev + 10;
       });
-    }, 300); 
-
+    }, 300);
 
     const timer = setTimeout(() => {
       setSpinning(false);
-      setPercent(0); 
-    }, 50); 
+      setPercent(0);
+    }, 50);
 
     // Cleanup on unmount
     return () => {
       clearInterval(interval);
       clearTimeout(timer);
     };
-  }, [pathname]); 
+  }, [pathname]);
 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="../public/images/emptystates-empty-cart.png"
+          type="image/png"
+        />
+        <title>Pantagonostis</title>
       </head>
       <body className="antialiased font-Inter">
         {/* Show loading spinner if page is loading */}
         {spinning ? (
           <div className="loader-container absolute top-1/2 left-[40%]  ">
-            <Image className="block mx-auto my-auto"  src={logo} alt="logo" />
-            <Spin className=" bg-white h-full w-full " size="large"  spinning={spinning} percent={percent} />
+            <Image className="block mx-auto my-auto" src={logo} alt="logo" />
+            <Spin
+              className=" bg-white h-full w-full "
+              size="large"
+              spinning={spinning}
+              percent={percent}
+            />
           </div>
         ) : (
           <>
             {/* Conditionally render Navbar and Footer */}
             {!isAuthRoute && <Navbar />}
-            
+
             {/* Render children (page content) */}
             {children}
 
